67a95a4b9896b8d81e338f32d5f518ecefafed00
Jaynil Savani <76048534+jaynilsavani@users.noreply.github.com>
2021-02-06 12:24:35 +0530

    refactor: minor code refactored

--- src/com/chat/client/ThreadClient.java
+++ src/com/chat/client/ThreadClient.java
@@ -4,11 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.SocketException;
 
 /**
  * Thread for clients
  */
 public class ThreadClient implements Runnable {
+
     private Socket socket;
     private BufferedReader cin;
 
@@ -24,7 +26,9 @@ public class ThreadClient implements Runnable {
                 String message = cin.readLine();
                 System.out.println(message);
             }
-        } catch (Exception exception) {
+        } catch (SocketException e) {
+            System.out.println("You left the chat-room");
+        } catch (IOException exception) {
             System.out.println(exception);
         } finally {
             try {
