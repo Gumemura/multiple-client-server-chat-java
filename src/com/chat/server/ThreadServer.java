af41480d865cdf0e6992dbd9ecf409ec9c5f4b00
Jaynil Savani <76048534+jaynilsavani@users.noreply.github.com>
2021-02-06 12:25:12 +0530

    refactor

--- src/com/chat/server/ThreadServer.java
+++ src/com/chat/server/ThreadServer.java
@@ -4,33 +4,49 @@ import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
+/**
+ * Thread for Server
+ */
 public class ThreadServer extends Thread {
+
     private Socket socket;
     private ArrayList<Socket> clients;
-    private PrintWriter output;
+    private HashMap<Socket, String> clientNameList;
 
-    public ThreadServer(Socket socket, ArrayList<Socket> clients) {
+    public ThreadServer(Socket socket, ArrayList<Socket> clients, HashMap<Socket, String> clientNameList) {
         this.socket = socket;
         this.clients = clients;
+        this.clientNameList = clientNameList;
     }
 
     @Override
     public void run() {
         try {
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            output = new PrintWriter(socket.getOutputStream(), true);
 
             while (true) {
                 String outputString = input.readLine();
                 if (outputString.equals("logout")) {
-                    break;
+                    throw new SocketException();
                 }
-                showMessageToAllClients(socket, outputString);
-                System.out.println(outputString);
+                if (!clientNameList.containsKey(socket)) {
+                    String[] messageString = outputString.split(":", 2);
+                    clientNameList.put(socket, messageString[0]);
+                    System.out.println(messageString[0] + messageString[1]);
+                    showMessageToAllClients(socket, messageString[0] + messageString[1]);
+                } else {
+                    System.out.println(outputString);
+                    showMessageToAllClients(socket, outputString);
+                }
             }
         } catch (SocketException e) {
+            String printMessage = clientNameList.get(socket) + " left the chat room";
+            System.out.println(printMessage);
+            showMessageToAllClients(socket, printMessage);
             clients.remove(socket);
+            clientNameList.remove(socket);
         } catch (Exception e) {
             System.out.println(e.getStackTrace());
         }
