9f4125fb5f94b91631310f9bca45f2189bdf567a
Kaynnan Felix <kaynnanap.m.felix@gmail.com>
2021-11-02 14:04:14 -0300

    Alterando interações no chat

--- Cliente.java
+++ Cliente.java
@@ -12,6 +12,9 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.swing.*;
 
 public class Cliente extends JFrame implements ActionListener, KeyListener {
@@ -20,34 +23,36 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
 	private JTextField txtMsg;
 	private JButton btnSend;
 	private JButton btnSair;
-	private JLabel lblHistorico;
+	private JLabel lblAnagrama;
 	private JLabel lblMsg;
 	private JPanel pnlContent;
 	private Socket socket;
-	private OutputStream ou ;
+	private OutputStream ou;
 	private Writer ouw;
 	private BufferedWriter bfw;
 	private JTextField txtIP;
 	private JTextField txtPorta;
 	private JTextField txtNome;
+	private LocalDateTime agora;
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-	public Cliente() throws IOException{
-	JLabel lblMessage = new JLabel("Verificar!");
-	txtIP = new JTextField("127.0.0.1");
-	txtPorta = new JTextField("12345");
-	txtNome = new JTextField("Cliente");
-	Object[] texts = {lblMessage, txtIP, txtPorta, txtNome };
-	JOptionPane.showMessageDialog(null, texts);
+	public Cliente() throws IOException {
+		JLabel lblMessage = new JLabel("Entrar no Chat Anagrama");
+		txtIP = new JTextField("127.0.0.1");
+		txtPorta = new JTextField("1003");
+		txtNome = new JTextField("Jogador");
+		Object[] texts = { lblMessage, txtIP, txtPorta, txtNome };
+		JOptionPane.showMessageDialog(null, texts);
 		pnlContent = new JPanel();
-		texto              = new JTextArea(10,20);
+		texto = new JTextArea(20, 20);
 		texto.setEditable(false);
-		texto.setBackground(new Color(240,240,240));
-		txtMsg                       = new JTextField(20);
-		lblHistorico     = new JLabel("Histórico");
-		lblMsg        = new JLabel("Mensagem");
-		btnSend                     = new JButton("Enviar");
+		texto.setBackground(new Color(240, 240, 240));
+		txtMsg = new JTextField(20);
+		lblAnagrama = new JLabel("Anagrama - Chat");
+		lblMsg = new JLabel("Mensagem");
+		btnSend = new JButton("Enviar");
 		btnSend.setToolTipText("Enviar Mensagem");
-		btnSair           = new JButton("Sair");
+		btnSair = new JButton("Sair");
 		btnSair.setToolTipText("Sair do Chat");
 		btnSend.addActionListener(this);
 		btnSair.addActionListener(this);
@@ -55,7 +60,7 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
 		txtMsg.addKeyListener(this);
 		JScrollPane scroll = new JScrollPane(texto);
 		texto.setLineWrap(true);
-		pnlContent.add(lblHistorico);
+		pnlContent.add(lblAnagrama);
 		pnlContent.add(scroll);
 		pnlContent.add(lblMsg);
 		pnlContent.add(txtMsg);
@@ -64,123 +69,146 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
 		pnlContent.setBackground(Color.LIGHT_GRAY);
 		texto.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.BLUE));
 		txtMsg.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.BLUE));
-		setTitle(txtNome.getText());
+		setTitle("Anagrama - " + txtNome.getText());
 		setContentPane(pnlContent);
 		setLocationRelativeTo(null);
 		setResizable(false);
-		setSize(250,300);
+		setSize(400, 600);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 	/***
-	  * Método usado para conectar no server socket, retorna IO Exception caso dê algum erro.
-	  * @throws IOException
-	  */
-	public void conectar() throws IOException{
+	 * Método usado para conectar no server socket, retorna IO Exception caso dê
+	 * algum erro.
+	 * 
+	 * @throws IOException
+	 */
+	public void conectar() throws IOException {
 
-	  socket = new Socket(txtIP.getText(),Integer.parseInt(txtPorta.getText()));
-	  ou = socket.getOutputStream();
-	  ouw = new OutputStreamWriter(ou);
-	  bfw = new BufferedWriter(ouw);
-	  bfw.write(txtNome.getText()+"\r\n");
-	  bfw.flush();
+		socket = new Socket(txtIP.getText(), Integer.parseInt(txtPorta.getText()));
+		ou = socket.getOutputStream();
+		ouw = new OutputStreamWriter(ou);
+		bfw = new BufferedWriter(ouw);
+		bfw.write(txtNome.getText() + " \r\n");
+		bfw.flush();
+
+		agora = LocalDateTime.now();
+		String horario = agora.format(formatter);
+		texto.append("[" + horario + " " + txtNome.getText() + "]: conectado. \r\n");
+		enviarMensagem("se conectou");
 	}
 
 	/***
-	  * Método usado para enviar mensagem para o server socket
-	  * @param msg do tipo String
-	  * @throws IOException retorna IO Exception caso dê algum erro.
-	  */
-	  public void enviarMensagem(String msg) throws IOException{
+	 * Método usado para enviar mensagem para o server socket
+	 * 
+	 * @param msg do tipo String
+	 * @throws IOException retorna IO Exception caso dê algum erro.
+	 */
+	public void enviarMensagem(String msg) throws IOException {
+		agora = LocalDateTime.now();
+		String horario = agora.format(formatter);
+		if (msg.toLowerCase().equals("/sair")) {
+			bfw.write(" Desconectado \r\n");
+			texto.append("[" + horario + " " + txtNome.getText() + "] - Desconectado \r\n");
+		} else if (msg.toLowerCase().equals("/anagrama")) {
 
-	    if(msg.equals("Sair")){
-	      bfw.write("Desconectado \r\n");
-	      texto.append("Desconectado \r\n");
-	    }else{
-	      bfw.write(msg+"\r\n");
-	      texto.append( txtNome.getText() + " diz -> " +         txtMsg.getText()+"\r\n");
-	    }
-	     bfw.flush();
-	     txtMsg.setText("");
+		} else if (msg.toLowerCase().equals("/pontos")) {
+
+		} else if (msg.toLowerCase().equals("/ajuda")) {
+			texto.append("Lista de Comandos: \n - Ajuda: /ajuda\n - Pontos: /pontos\n - Anagrama: /anagrama \r\n");
+		} else if (msg.toLowerCase().equals("se conectou")) {
+			bfw.write(msg + " \r\n");
+		} else {
+			bfw.write(msg + " \r\n");
+			texto.append("[" + horario + " " + txtNome.getText() + "]: " + txtMsg.getText() + "\r\n");
+		}
+		bfw.flush();
+		txtMsg.setText("");
 	}
 
 	/**
 	 * Método usado para receber mensagem do servidor
+	 * 
 	 * @throws IOException retorna IO Exception caso dê algum erro.
 	 */
-	public void escutar() throws IOException{
+	public void escutar() throws IOException {
 
-	   InputStream in = socket.getInputStream();
-	   InputStreamReader inr = new InputStreamReader(in);
-	   BufferedReader bfr = new BufferedReader(inr);
-	   String msg = "";
+		InputStream in = socket.getInputStream();
+		InputStreamReader inr = new InputStreamReader(in);
+		BufferedReader bfr = new BufferedReader(inr);
+		String msg = "";
 
-	    while(!"Sair".equalsIgnoreCase(msg))
+		while (!"/sair".equalsIgnoreCase(msg))
 
-	       if(bfr.ready()){
-	         msg = bfr.readLine();
-	       if(msg.equals("Sair"))
-	         texto.append("Servidor caiu! \r\n");
-	        else
-	         texto.append(msg+"\r\n");
-	        }
+			if (bfr.ready()) {
+				msg = bfr.readLine();
+				if (msg.toLowerCase().equals("/sair"))
+					texto.append("Servidor caiu! \r\n");
+				else
+					texto.append(msg + "\r\n");
+			}
 	}
 
 	/***
-	  * Método usado quando o usuário clica em sair
-	  * @throws IOException retorna IO Exception caso dê algum erro.
-	  */
-	  public void sair() throws IOException{
+	 * Método usado quando o usuário clica em sair
+	 * 
+	 * @throws IOException retorna IO Exception caso dê algum erro.
+	 */
+	public void sair() throws IOException {
 
-	   enviarMensagem("Sair");
-	   bfw.close();
-	   ouw.close();
-	   ou.close();
-	   socket.close();
+		enviarMensagem("/sair");
+		bfw.close();
+		ouw.close();
+		ou.close();
+		socket.close();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-	  try {
-	     if(e.getActionCommand().equals(btnSend.getActionCommand()))
-	        enviarMensagem(txtMsg.getText());
-	     else
-	        if(e.getActionCommand().equals(btnSair.getActionCommand()))
-	        sair();
-	     } catch (IOException e1) {
-	          // TODO Auto-generated catch block
-	          e1.printStackTrace();
-	     }
+		try {
+			if (e.getActionCommand().equals(btnSend.getActionCommand()))
+				enviarMensagem(txtMsg.getText());
+			else if (e.getActionCommand().equals(btnSair.getActionCommand()))
+				sair();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 
-	    if(e.getKeyCode() == KeyEvent.VK_ENTER){
-	       try {
-	          enviarMensagem(txtMsg.getText());
-	       } catch (IOException e1) {
-	           // TODO Auto-generated catch block
-	           e1.printStackTrace();
-	       }
-	   }
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			try {
+				enviarMensagem(txtMsg.getText());
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			try {
+				sair();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
-	  // TODO Auto-generated method stub
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
-	  // TODO Auto-generated method stub
 	}
 
-	public static void main(String []args) throws IOException{
+	public static void main(String[] args) throws IOException {
 
-	   Cliente app = new Cliente();
-	   app.conectar();
-	   app.escutar();
+		Cliente app = new Cliente();
+		app.conectar();
+		app.escutar();
 	}
 }
--- Servidor.java
+++ Servidor.java
@@ -8,107 +8,112 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class Servidor extends Thread {
-	private static ArrayList<BufferedWriter>clientes;
+	private static ArrayList<BufferedWriter> clientes;
 	private static ServerSocket server;
 	private String nome;
 	private Socket con;
 	private InputStream in;
 	private InputStreamReader inr;
 	private BufferedReader bfr;
+	private LocalDateTime agora;
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
 	/**
-	  * Método construtor
-	  * @param com do tipo Socket
-	  */
-	public Servidor(Socket con){
-	   this.con = con;
-	   try {
-	         in  = con.getInputStream();
-	         inr = new InputStreamReader(in);
-	          bfr = new BufferedReader(inr);
-	   } catch (IOException e) {
-	          e.printStackTrace();
-	   }
+	 * Método construtor
+	 * 
+	 * @param com do tipo Socket
+	 */
+	public Servidor(Socket con) {
+		this.con = con;
+		try {
+			in = con.getInputStream();
+			inr = new InputStreamReader(in);
+			bfr = new BufferedReader(inr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+
 	/**
-	  * Método run
-	  */
-	public void run(){
-	  try{
-	    String msg;
-	    OutputStream ou =  this.con.getOutputStream();
-	    Writer ouw = new OutputStreamWriter(ou);
-	    BufferedWriter bfw = new BufferedWriter(ouw);
-	    clientes.add(bfw);
-	    nome = msg = bfr.readLine();
+	 * Método run
+	 */
+	public void run() {
+		try {
+			String msg;
+			OutputStream ou = this.con.getOutputStream();
+			Writer ouw = new OutputStreamWriter(ou);
+			BufferedWriter bfw = new BufferedWriter(ouw);
+			clientes.add(bfw);
+			nome = msg = bfr.readLine();
 
-	    while(!"Sair".equalsIgnoreCase(msg) && msg != null)
-	      {
-	       msg = bfr.readLine();
-	       sendToAll(bfw, msg);
-	       System.out.println(msg);
-	       }
+			while (!"Sair".equalsIgnoreCase(msg) && msg != null) {
+				msg = bfr.readLine();
+				sendToAll(bfw, msg);
+				System.out.println(msg);
+			}
 
-	   }catch (Exception e) {
-	     e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 
-	   }
+		}
 	}
 
 	/***
 	 * Método usado para enviar mensagem para todos os clients
+	 * 
 	 * @param bwSaida do tipo BufferedWriter
-	 * @param msg do tipo String
+	 * @param msg     do tipo String
 	 * @throws IOException
 	 */
-	public void sendToAll(BufferedWriter bwSaida, String msg) throws  IOException
-	{
-	  BufferedWriter bwS;
+	public void sendToAll(BufferedWriter bwSaida, String msg) throws IOException {
+		BufferedWriter bwS;
+		agora = LocalDateTime.now();
+		String horario = agora.format(formatter);
 
-	  for(BufferedWriter bw : clientes){
-	   bwS = (BufferedWriter)bw;
-	   if(!(bwSaida == bwS)){
-	     bw.write(nome + " -> " + msg+"\r\n");
-	     bw.flush();
-	   }
-	  }
+		for (BufferedWriter bw : clientes) {
+			bwS = (BufferedWriter) bw;
+			if (!(bwSaida == bwS)) {
+				bw.write("[" + horario + " " + nome + "]: " + msg + "\r\n");
+				bw.flush();
+			}
+		}
 	}
 
 	/***
-	   * Método main
-	   * @param args
-	   */
-	public static void main(String []args) {
+	 * Método main
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
 
-	  try{
-	    //Cria os objetos necessário para instânciar o servidor
-	    JLabel lblMessage = new JLabel("Porta do Servidor:");
-	    JTextField txtPorta = new JTextField("12345");
-	    Object[] texts = {lblMessage, txtPorta };
-	    JOptionPane.showMessageDialog(null, texts);
-	    server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
-	    clientes = new ArrayList<BufferedWriter>();
-	    JOptionPane.showMessageDialog(null,"Servidor ativo na porta: "+
-	    txtPorta.getText());
+		try {
+			// Cria os objetos necessário para instânciar o servidor
+			JLabel lblMessage = new JLabel("Porta do Servidor:");
+			JTextField txtPorta = new JTextField("1003");
+			Object[] texts = { lblMessage, txtPorta };
+			JOptionPane.showMessageDialog(null, texts);
+			server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
+			clientes = new ArrayList<BufferedWriter>();
+			JOptionPane.showMessageDialog(null, "Servidor ativo na porta: " + txtPorta.getText());
 
-	     while(true){
-	       System.out.println("Aguardando conexão...");
-	       Socket con = server.accept();
-	       System.out.println("Cliente conectado...");
-	       Thread t = new Servidor(con);
-	        t.start();
-	    }
+			while (true) {
+				System.out.println("Aguardando jogador conectar...");
+				Socket con = server.accept();
+				System.out.println(con.getInetAddress() + " conectado...");
+				Thread t = new Servidor(con);
+				t.start();
+			}
 
-	  }catch (Exception e) {
-
-	    e.printStackTrace();
-	  }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}// Fim do método main
-} //Fim da classe
-
+} // Fim da classe
