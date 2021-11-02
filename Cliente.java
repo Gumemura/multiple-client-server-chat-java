feaf746d3d6c17613657c8d06dd21200701c4645
Gumemura <gumemura123@gmail.com>
2021-11-02 12:52:40 -0300

    fixing indentation

--- Cliente.java
+++ Cliente.java
@@ -32,22 +32,22 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
 	private JTextField txtNome;
 
 	public Cliente() throws IOException{
-	JLabel lblMessage = new JLabel("Verificar!");
-	txtIP = new JTextField("127.0.0.1");
-	txtPorta = new JTextField("12345");
-	txtNome = new JTextField("Cliente");
-	Object[] texts = {lblMessage, txtIP, txtPorta, txtNome };
-	JOptionPane.showMessageDialog(null, texts);
+		JLabel lblMessage = new JLabel("Verificar!");
+		txtIP = new JTextField("127.0.0.1");
+		txtPorta = new JTextField("12345");
+		txtNome = new JTextField("Cliente");
+		Object[] texts = {lblMessage, txtIP, txtPorta, txtNome };
+		JOptionPane.showMessageDialog(null, texts);
 		pnlContent = new JPanel();
-		texto              = new JTextArea(10,20);
+		texto = new JTextArea(10,20);
 		texto.setEditable(false);
 		texto.setBackground(new Color(240,240,240));
-		txtMsg                       = new JTextField(20);
-		lblHistorico     = new JLabel("Histórico");
-		lblMsg        = new JLabel("Mensagem");
-		btnSend                     = new JButton("Enviar");
+		txtMsg = new JTextField(20);
+		lblHistorico = new JLabel("Histórico");
+		lblMsg = new JLabel("Mensagem");
+		btnSend = new JButton("Enviar");
 		btnSend.setToolTipText("Enviar Mensagem");
-		btnSair           = new JButton("Sair");
+		btnSair = new JButton("Sair");
 		btnSair.setToolTipText("Sair do Chat");
 		btnSend.addActionListener(this);
 		btnSair.addActionListener(this);
@@ -67,8 +67,8 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
 		setTitle(txtNome.getText());
 		setContentPane(pnlContent);
 		setLocationRelativeTo(null);
-		setResizable(false);
-		setSize(250,300);
+		setResizable(true);
+		setSize(250, 320);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
@@ -78,13 +78,12 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
 	  * @throws IOException
 	  */
 	public void conectar() throws IOException{
-
-	  socket = new Socket(txtIP.getText(),Integer.parseInt(txtPorta.getText()));
-	  ou = socket.getOutputStream();
-	  ouw = new OutputStreamWriter(ou);
-	  bfw = new BufferedWriter(ouw);
-	  bfw.write(txtNome.getText()+"\r\n");
-	  bfw.flush();
+		socket = new Socket(txtIP.getText(),Integer.parseInt(txtPorta.getText()));
+		ou = socket.getOutputStream();
+		ouw = new OutputStreamWriter(ou);
+		bfw = new BufferedWriter(ouw);
+		bfw.write(txtNome.getText()+"\r\n");
+		bfw.flush();
 	}
 
 	/***
@@ -92,17 +91,16 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
 	  * @param msg do tipo String
 	  * @throws IOException retorna IO Exception caso dê algum erro.
 	  */
-	  public void enviarMensagem(String msg) throws IOException{
-
-	    if(msg.equals("Sair")){
-	      bfw.write("Desconectado \r\n");
-	      texto.append("Desconectado \r\n");
-	    }else{
-	      bfw.write(msg+"\r\n");
-	      texto.append( txtNome.getText() + " diz -> " +         txtMsg.getText()+"\r\n");
-	    }
-	     bfw.flush();
-	     txtMsg.setText("");
+	public void enviarMensagem(String msg) throws IOException{
+		if(msg.equals("Sair")){
+			bfw.write("Desconectado \r\n");
+			texto.append("Desconectado \r\n");
+		}else{
+			bfw.write(msg+"\r\n");
+			texto.append( txtNome.getText() + " diz -> " + txtMsg.getText()+"\r\n");
+		}
+		bfw.flush();
+		txtMsg.setText("");
 	}
 
 	/**
@@ -116,54 +114,51 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
 	   BufferedReader bfr = new BufferedReader(inr);
 	   String msg = "";
 
-	    while(!"Sair".equalsIgnoreCase(msg))
-
-	       if(bfr.ready()){
-	         msg = bfr.readLine();
-	       if(msg.equals("Sair"))
-	         texto.append("Servidor caiu! \r\n");
-	        else
-	         texto.append(msg+"\r\n");
-	        }
+		while(!"Sair".equalsIgnoreCase(msg))
+		  	if(bfr.ready()){
+				msg = bfr.readLine();
+		  	if(msg.equals("Sair"))
+				texto.append("Servidor caiu! \r\n");
+			else
+				texto.append(msg+"\r\n");
+			}
 	}
 
 	/***
 	  * Método usado quando o usuário clica em sair
 	  * @throws IOException retorna IO Exception caso dê algum erro.
 	  */
-	  public void sair() throws IOException{
-
-	   enviarMensagem("Sair");
-	   bfw.close();
-	   ouw.close();
-	   ou.close();
-	   socket.close();
+	public void sair() throws IOException{
+		enviarMensagem("Sair");
+		bfw.close();
+		ouw.close();
+		ou.close();
+		socket.close();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
 	  try {
-	     if(e.getActionCommand().equals(btnSend.getActionCommand()))
-	        enviarMensagem(txtMsg.getText());
-	     else
-	        if(e.getActionCommand().equals(btnSair.getActionCommand()))
-	        sair();
-	     } catch (IOException e1) {
-	          // TODO Auto-generated catch block
-	          e1.printStackTrace();
-	     }
+		if(e.getActionCommand().equals(btnSend.getActionCommand()))
+			enviarMensagem(txtMsg.getText());
+		else
+			if(e.getActionCommand().equals(btnSair.getActionCommand()))
+				sair();
+		 	} catch (IOException e1) {
+			 	// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 	}
 	@Override
 	public void keyPressed(KeyEvent e) {
 
-	    if(e.getKeyCode() == KeyEvent.VK_ENTER){
-	       try {
-	          enviarMensagem(txtMsg.getText());
-	       } catch (IOException e1) {
-	           // TODO Auto-generated catch block
-	           e1.printStackTrace();
-	       }
+		if(e.getKeyCode() == KeyEvent.VK_ENTER){
+		   try {
+				enviarMensagem(txtMsg.getText());
+		   } catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+		   }
 	   }
 	}
 
@@ -178,7 +173,6 @@ public class Cliente extends JFrame implements ActionListener, KeyListener {
 	}
 
 	public static void main(String []args) throws IOException{
-
 	   Cliente app = new Cliente();
 	   app.conectar();
 	   app.escutar();
--- Servidor.java
+++ Servidor.java
@@ -9,6 +9,8 @@ import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+
+//UI
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
@@ -22,43 +24,43 @@ public class Servidor extends Thread {
 	private InputStreamReader inr;
 	private BufferedReader bfr;
 
-	/**
-	  * Método construtor
-	  * @param com do tipo Socket
-	  */
+	/*
+	*
+	* Método construtor
+	* @param com do tipo Socket
+	*/
 	public Servidor(Socket con){
 	   this.con = con;
 	   try {
-	         in  = con.getInputStream();
-	         inr = new InputStreamReader(in);
-	          bfr = new BufferedReader(inr);
+	        in = con.getInputStream();
+	        inr = new InputStreamReader(in);
+	        bfr = new BufferedReader(inr);
 	   } catch (IOException e) {
-	          e.printStackTrace();
+	        e.printStackTrace();
 	   }
 	}
 	/**
 	  * Método run
 	  */
 	public void run(){
-	  try{
-	    String msg;
-	    OutputStream ou =  this.con.getOutputStream();
-	    Writer ouw = new OutputStreamWriter(ou);
-	    BufferedWriter bfw = new BufferedWriter(ouw);
-	    clientes.add(bfw);
-	    nome = msg = bfr.readLine();
+		try{
+		    String msg;
+		    OutputStream ou =  this.con.getOutputStream();
+		    Writer ouw = new OutputStreamWriter(ou);
+		    BufferedWriter bfw = new BufferedWriter(ouw);
+		    clientes.add(bfw);
+		    nome = msg = bfr.readLine();
 
-	    while(!"Sair".equalsIgnoreCase(msg) && msg != null)
-	      {
-	       msg = bfr.readLine();
-	       sendToAll(bfw, msg);
-	       System.out.println(msg);
-	       }
+		    while(!"Sair".equalsIgnoreCase(msg) && msg != null)
+		    {
+		    	msg = bfr.readLine();
+				sendToAll(bfw, msg);
+				System.out.println(msg);
+		    }
 
-	   }catch (Exception e) {
-	     e.printStackTrace();
-
-	   }
+	   	}catch (Exception e) {
+	    	e.printStackTrace();
+	   	}
 	}
 
 	/***
@@ -69,46 +71,41 @@ public class Servidor extends Thread {
 	 */
 	public void sendToAll(BufferedWriter bwSaida, String msg) throws  IOException
 	{
-	  BufferedWriter bwS;
+		BufferedWriter bwS;
 
-	  for(BufferedWriter bw : clientes){
-	   bwS = (BufferedWriter)bw;
-	   if(!(bwSaida == bwS)){
-	     bw.write(nome + " -> " + msg+"\r\n");
-	     bw.flush();
-	   }
-	  }
+		for(BufferedWriter bw : clientes){
+			bwS = (BufferedWriter)bw;
+   			if(!(bwSaida == bwS)){
+    			bw.write(nome + " -> " + msg+"\r\n");
+	     		bw.flush();
+	   		}
+	  	}
 	}
 
 	/***
 	   * Método main
-	   * @param args
-	   */
+	   * @param args 
+	*/
 	public static void main(String []args) {
-
-	  try{
-	    //Cria os objetos necessário para instânciar o servidor
-	    JLabel lblMessage = new JLabel("Porta do Servidor:");
-	    JTextField txtPorta = new JTextField("12345");
-	    Object[] texts = {lblMessage, txtPorta };
-	    JOptionPane.showMessageDialog(null, texts);
-	    server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
-	    clientes = new ArrayList<BufferedWriter>();
-	    JOptionPane.showMessageDialog(null,"Servidor ativo na porta: "+
-	    txtPorta.getText());
-
-	     while(true){
-	       System.out.println("Aguardando conexão...");
-	       Socket con = server.accept();
-	       System.out.println("Cliente conectado...");
-	       Thread t = new Servidor(con);
-	        t.start();
-	    }
-
-	  }catch (Exception e) {
-
-	    e.printStackTrace();
-	  }
+	  	try{
+		    //Cria os objetos necessário para instânciar o servidor
+		    JLabel lblMessage = new JLabel("Porta do Servidor:");
+		    JTextField txtPorta = new JTextField("12345");
+		    Object[] texts = {lblMessage, txtPorta};
+		    JOptionPane.showMessageDialog(null, texts);
+		    server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
+		    clientes = new ArrayList<BufferedWriter>();
+		    JOptionPane.showMessageDialog(null,"Servidor ativo na porta: "+ txtPorta.getText());
+		    while(true){
+		       System.out.println("Aguardando conexão...");
+		       Socket con = server.accept();
+		       System.out.println("Cliente conectado...");
+		       Thread t = new Servidor(con);
+		       t.start();
+		    }
+	  	}catch (Exception e) {
+	  		e.printStackTrace();
+	  	}
 	}// Fim do método main
 } //Fim da classe
 
